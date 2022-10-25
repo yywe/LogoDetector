d001bb0120001e7638e1f9054b72e8a79c5f0c29
weiy49 <weiy49@mcmaster.ca>
2022-10-24 22:32:39 -0400

    update

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # LogoDetector
 
-Simple Demo
+Simple demos for logo detection using template maching and feature matching based on open-cv.
+
+## Template Matching
+
+Template matching is a naive solution, the basic idea is to slide the template image inside the source image, and for each slide step compute and memorize the maximum similarity, which is used as the final output.
+
+Pros:
+* easy, fast, straightforward
+* can handle variations in translation, scaling (perhaps some kind of transparency or minor distotion)
+
+Cons:
+* due to its nature, cannot handle rotation. (we can similarly rotate the template as a workround)
+* template matching itself does not tell us if an object appear in an image or not. Need to tune a threshold parameter on the correlation coefficient for this purpose
+
+Demo result:
+![Result using template matching](images/nbc-result-temp-match.png)
+
+
+## Key Matching
+
+Different from template matching which directly slides the template image, key point matching first extracts features (keypoints and its description), and then tries to establish a mapping between the source features and template features.
+
+Pros:
+* the major advantage is that this solution is local feature invariant, robust to rotation, scaling, etc.
+
+Cons:
+* sometimes, it does not work well due to mismatch or not enough correct match. (try test with cnn-news-small.png)
+* also this alg has some kind of randomness due to random sample consensus (RANSAC)
+* also need to tune other params like how many good matches suffice to infer an object exist
+
+Demo result:
+![Result using key point matching](images/nbc-result-keypoint-match.png)
+
+# References
+
+* https://docs.opencv.org/4.x/d1/de0/tutorial_py_feature_homography.html
+* https://pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/
+
--- kps-match.py
+++ kps-match.py
@@ -2,103 +2,100 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 12 22:24:18 2022
-
 Use key point matching to search for logos
-Ref: https://docs.opencv.org/3.4/d1/de0/tutorial_py_feature_homography.html
-
-Pros:
-    major advantage: 
-        local feature invariant, robust to rotation, scaling, etc.
-Cons:
-    sometimes, it does not work well due to mismatch. (try test with cnn-news-small.png)
-    also this alg has some kind of randomness due to Random sample consensus (RANSAC)
-    also need to tune the param like how many good matches suffice to infer an object exist
 """
 
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-# define input images
-templatepath = "./images/nbc-logo.png"
-sourceimgpath = "./images/nbc-small.png"
-
-
-# feature type, can be SIFT or ORB
-feature_type = "ORB"
-
-# minimal match count to be valid
-MIN_MATCH_COUNT = 10
-
-# max number of features note this param is imporant
-nfeatures = 2000
-
-# load template image (as gray mode)
-template = cv2.imread(templatepath, 0)
-plt.imshow(template)
-plt.show()
-
-# load source image, keep a copy of original BGR format
-image = cv2.imread(sourceimgpath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-plt.imshow(gray)
-plt.show()
-
-feature_detector = None
-index_params = None
-if feature_type == "ORB":
-    FLANN_INDEX_LSH = 6
-    index_params= dict(algorithm = FLANN_INDEX_LSH,
-                   table_number = 6, # 12
-                   key_size = 12,     # 20
-                   multi_probe_level = 1) #2
-    feature_detector = cv2.ORB_create(nfeatures)
-elif feature_type == "SIFT":
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    feature_detector = cv2.SIFT_create(nfeatures)
-else:
-    print("unsupported feature type")
+def search_template(sourceimgpath, templatepath, feature_type = "ORB", min_match_count = 10, nfeatures = 2000,  plot_match = True, outfile='./images/result.png'):
+    """search template in an image using key points match
     
-search_params = dict(checks = 50)
+    Parameters
+    ----------
+    sourceimgpath : string
+        source image path.
+    templatepath : string
+        template image path.
+    feature_type : string, optional
+        feature type, can be SIFT or ORB. The default is "ORB".
+    min_match_count : string, optional
+        minimal number of key point matches for valid detect. The default is 10.
+    nfeatures : string, optional
+        max number of key point used. The default is 2000.
+    outfile : string, optional
+        output path. The default is './images/result.png'.
 
-kp_template, des_template = feature_detector.detectAndCompute(template, None)
-kp_image, des_image = feature_detector.detectAndCompute(gray, None)
+    Returns
+    -------
+    None.
 
-matcher = cv2.FlannBasedMatcher(index_params, search_params)
-matches = matcher.knnMatch(des_template,des_image,k=2)
-
-# get good match
-good = []
-for m,n in matches:
-    if m.distance < 0.7*n.distance:
-        good.append(m)
-
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp_template[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp_image[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
-    h,w = template.shape
-    # get the 4 corner points of the template
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    # get the new corners applying the transformation matrix
-    dst = cv2.perspectiveTransform(pts,M)
-    # draw the box
-    image = cv2.polylines(image,[np.int32(dst)],True,(0, 0, 255), thickness=30, lineType=cv2.LINE_AA)
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.show()
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-    matchesMask = None
-
-"""
-# draw the match pairs
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+    """
+    
+    template = cv2.imread(templatepath, 0)
+    image = cv2.imread(sourceimgpath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    feature_detector = None
+    index_params = None
+    if feature_type == "ORB":
+        FLANN_INDEX_LSH = 6
+        index_params= dict(algorithm = FLANN_INDEX_LSH,
+                       table_number = 6, 
+                       key_size = 12,   
+                       multi_probe_level = 1)
+        feature_detector = cv2.ORB_create(nfeatures)
+    elif feature_type == "SIFT":
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        feature_detector = cv2.SIFT_create(nfeatures)
+    else:
+        print("unsupported feature type")
+        
+    search_params = dict(checks = 50)
+    
+    kp_template, des_template = feature_detector.detectAndCompute(template, None)
+    kp_image, des_image = feature_detector.detectAndCompute(gray, None)
+    
+    matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = matcher.knnMatch(des_template,des_image,k=2)
+    
+    # get good match count
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+    # valid detect
+    if len(good)> min_match_count:
+        src_pts = np.float32([ kp_template[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp_image[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+        h,w = template.shape
+        # get the 4 corner points of the template
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        # get the new corners applying the transformation matrix
+        dst = cv2.perspectiveTransform(pts,M)
+        # draw the box
+        image = cv2.polylines(image,[np.int32(dst)],True,(0, 0, 255), thickness=30, lineType=cv2.LINE_AA)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
+        cv2.imwrite(outfile, image)
+    else:
+        print( "Not enough matches found - {}/{}".format(len(good), min_match_count) )
+        matchesMask = None
+        
+    if plot_match:
+        draw_params = dict(matchColor = (0,255,0),
                    singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
+                   matchesMask = matchesMask,
                    flags = 2)
-matchimg = cv2.drawMatches(template,kp_template,image,kp_image,good,None,**draw_params)
-plt.imshow(cv2.cvtColor(matchimg, cv2.COLOR_BGR2RGB))
-plt.show()
-"""
+        matchimg = cv2.drawMatches(template,kp_template,image,kp_image,good,None,**draw_params)
+        plt.imshow(cv2.cvtColor(matchimg, cv2.COLOR_BGR2RGB))
+        plt.show()
+        
+if __name__== '__main__':
+    templatepath = "./images/nbc-logo.png"
+    sourceimgpath = "./images/nbc-rotated.png"
+    search_template(sourceimgpath, templatepath, outfile='./images/nbc-result-keypoint-match.png')
